f9c7d94fc4e18ebfbd7c3864b734cfcf4e3d36ad
Sohn Young Jin <sonkim1001@naver.com>
2021-07-31 21:04:26 +0900

    Create new activities
    New activity list: IntroActivity, LoginActivity, QRRecognitionActivity, LobbyActivity, OrderListActivity, SettingActivity, CardActivity, OrderingActivity, PayActivity.

---
+++ app/src/main/java/com/uof/uof_mobile/LobbyActivity.java
@@ -0,0 +1,14 @@
+package com.uof.uof_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
