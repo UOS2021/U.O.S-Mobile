e497fd5379851a2afedbd0470e736c4f826f9f39
Sohn Young Jin <sonkim1001@naver.com>
2021-08-03 18:57:08 +0900

    Add sub class to Constants class
    Create Network and Pattern class in Constants class. The network class contains constants used in communication. The pattern class contains constants for string pattern results through regular expressions.

--- app/src/main/java/com/uof/uof_mobile/Constants.java
+++ app/src/main/java/com/uof/uof_mobile/Constants.java
@@ -1,7 +1,17 @@
 package com.uof.uof_mobile;
 
 public class Constants {
-    public final static int SOCKET_MAX_RECV_SIZE = 1000;
-    public final static String NETWORK_SERVER_IP = "0.0.0.0";
-    public final static int NETWORK_SERVER_PORT = 0;
+    public class Network{
+        public final static int SOCKET_MAX_RECV_SIZE = 1000;
+
+        public final static String EXTERNAL_SERVER_IP = "211.217.202.157";
+        public final static int EXTERNAL_SERVER_PORT = 8080;
+    }
+
+    public class Pattern {
+        public final static int OK = 0;
+        public final static int LENGTH_SHORT = 1;
+        public final static int LENGTH_LONG = 2;
+        public final static int NOT_ALLOWED_CHARACTER = 3;
+    }
 }
