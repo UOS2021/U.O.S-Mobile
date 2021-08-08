45729745a6644343f091b90664027800a35e5aba
Sohn Young Jin <sonkim1001@naver.com>
2021-08-08 19:30:18 +0900

    Add processing function according to the selected setting
    Implementation of functions to change phone number, change password, and withdrawal.

--- app/src/main/java/com/uof/uof_mobile/fragment/SettingPreferenceFragment.java
+++ app/src/main/java/com/uof/uof_mobile/fragment/SettingPreferenceFragment.java
@@ -1,29 +1,35 @@
 package com.uof.uof_mobile.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.widget.Toast;
 
 import com.uof.uof_mobile.R;
+import com.uof.uof_mobile.activity.ChangePhoneActivity;
+import com.uof.uof_mobile.activity.ChangePwActivity;
+import com.uof.uof_mobile.activity.WithdrawalActivity;
 
 
 public class SettingPreferenceFragment extends PreferenceFragment {
+    private Context context;
     private SharedPreferences prefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        context = getActivity().getApplicationContext();
+
         addPreferencesFromResource(R.xml.settings_preference);
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         SharedPreferences.OnSharedPreferenceChangeListener prefsListener = (sharedPreferences, key) -> {
             if (key.equals("vibration") && sharedPreferences.getBoolean("vibration", false) == true) {
-                Vibrator vibrator = (Vibrator) getActivity().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
                 vibrator.vibrate(500);
             }
         };
@@ -35,28 +41,28 @@ public class SettingPreferenceFragment extends PreferenceFragment {
 
         // 비밀번호 변경이 눌렸을 경우
         btnChangePw.setOnPreferenceClickListener(preference -> {
-            Toast.makeText(getActivity().getApplicationContext(), "비밀번호 변경", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(context, ChangePwActivity.class));
 
             return false;
         });
 
         // 전화번호 변경이 눌렸을 경우
         btnChangePhone.setOnPreferenceClickListener(preference -> {
-            Toast.makeText(getActivity().getApplicationContext(), "핸드폰 번호 수정", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(context, ChangePhoneActivity.class));
 
             return false;
         });
 
         // 로그아웃이 눌렸을 경우
         btnLogout.setOnPreferenceClickListener(preference -> {
-            Toast.makeText(getActivity().getApplicationContext(), "로그아웃", Toast.LENGTH_SHORT).show();
+
 
             return false;
         });
 
         // 회원탈퇴가 눌렸을 경우
         btnWithdrawal.setOnPreferenceClickListener(preference -> {
-            Toast.makeText(getActivity().getApplicationContext(), "탈퇴", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(context, WithdrawalActivity.class));
 
             return false;
         });
