385af6cdf5614b687945492badb555e8f1b56baa
Sohn Young Jin <sonkim1001@naver.com>
2021-08-14 15:41:54 +0900

    Add intent for go to OrderingActivity
    Go to OrderingActivity when U.O.F QR code recognized.

--- app/src/main/java/com/uof/uof_mobile/activity/IntroActivity.java
+++ app/src/main/java/com/uof/uof_mobile/activity/IntroActivity.java
@@ -1,6 +1,7 @@
 package com.uof.uof_mobile.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,24 +19,21 @@ public class IntroActivity extends AppCompatActivity {
     }
 
     private void init() {
-        Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
-        startActivity(intent);
-
-        /* QR 실행 및 소켓 통신 테스트
-        Intent socketTestActivity = new Intent(LoginActivity.this, SocketTestActivity.class);
-
-        if(Intent.ACTION_VIEW.equals(getIntent().getAction())){
+        if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
             Uri uri = getIntent().getData();
 
-            if(uri != null){
-                String targetIp = uri.getQueryParameter("targetIp");
-                int targetPort = Integer.parseInt(uri.getQueryParameter("targetPort"));
-
-                socketTestActivity.putExtra("targetIp", targetIp);
-                socketTestActivity.putExtra("targetPort", targetPort);
+            if (uri == null) {
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(IntroActivity.this, OrderingActivity.class);
+                intent.putExtra("targetIp", ((Uri) uri).getQueryParameter("targetIp"));
+                intent.putExtra("targetPort", Integer.parseInt(uri.getQueryParameter("targetPort")));
+                startActivity(intent);
             }
+        } else {
+            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+            startActivity(intent);
         }
-        startActivity(socketTestActivity);
-         */
     }
 }
