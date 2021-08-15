bc19c6d289668fcf3be09ce714acc1c1f662a72a
Sohn Young Jin <sonkim1001@naver.com>
2021-08-15 22:34:41 +0900

    Add auto login logic in IntroActivity

--- app/src/main/java/com/uof/uof_mobile/activity/IntroActivity.java
+++ app/src/main/java/com/uof/uof_mobile/activity/IntroActivity.java
@@ -1,12 +1,13 @@
 package com.uof.uof_mobile.activity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.uof.uof_mobile.Constants;
 import com.uof.uof_mobile.R;
+import com.uof.uof_mobile.manager.SharedPreferenceManager;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -19,21 +20,35 @@ public class IntroActivity extends AppCompatActivity {
     }
 
     private void init() {
-        if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
-            Uri uri = getIntent().getData();
-
-            if (uri == null) {
-                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
-                startActivity(intent);
-            } else {
-                Intent intent = new Intent(IntroActivity.this, RestaurantOrderingActivity.class);
-                intent.putExtra("targetIp", ((Uri) uri).getQueryParameter("targetIp"));
-                intent.putExtra("targetPort", Integer.parseInt(uri.getQueryParameter("targetPort")));
-                startActivity(intent);
-            }
-        } else {
-            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
-            startActivity(intent);
+        SharedPreferenceManager.open(IntroActivity.this, Constants.SharedPreference.APP_DATA);
+        if (SharedPreferenceManager.load(Constants.SharedPreference.IS_FIRST, true) == true) {
+            SharedPreferenceManager.save(Constants.SharedPreference.IS_LOGINED, false);
+            SharedPreferenceManager.save(Constants.SharedPreference.USER_ID, "");
+            SharedPreferenceManager.save(Constants.SharedPreference.USER_PW, "");
+            SharedPreferenceManager.save(Constants.SharedPreference.USER_TYPE, "");
+            SharedPreferenceManager.save(Constants.SharedPreference.IS_FIRST, false);
         }
+
+        Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+        intent.putExtra("isLogined", SharedPreferenceManager.load(Constants.SharedPreference.IS_LOGINED, false));
+        startActivity(intent);
+        finish();
+
+//        if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
+//            Uri uri = getIntent().getData();
+//
+//            if (uri == null) {
+//                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//            } else {
+//                Intent intent = new Intent(IntroActivity.this, RestaurantOrderingActivity.class);
+//                intent.putExtra("targetIp", ((Uri) uri).getQueryParameter("targetIp"));
+//                intent.putExtra("targetPort", Integer.parseInt(uri.getQueryParameter("targetPort")));
+//                startActivity(intent);
+//            }
+//        } else {
+//            Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+//            startActivity(intent);
+//        }
     }
 }
