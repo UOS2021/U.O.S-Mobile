c83bb8e7f5038947c25045dcc82e14042372157e
Sohn Young Jin <sonkim1001@naver.com>
2021-08-16 16:10:03 +0900

    Extract product information from imported data
    Separation of data received from Pos into products and sets.
    For categories, add to the top ChipGroup.

--- app/src/main/java/com/uof/uof_mobile/activity/RestaurantOrderingActivity.java
+++ app/src/main/java/com/uof/uof_mobile/activity/RestaurantOrderingActivity.java
@@ -7,23 +7,41 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatTextView;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
 import com.uof.uof_mobile.Constants;
 import com.uof.uof_mobile.R;
-import com.uof.uof_mobile.manager.SocketManager;
+import com.uof.uof_mobile.dialog.SelectProductDialog;
+import com.uof.uof_mobile.manager.RestaurantBascketManager;
+import com.uof.uof_mobile.recyclerview.RestaurantOrderingAdapter;
+import com.uof.uof_mobile.recyclerview.RestaurantOrderingProductItem;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
+
 public class RestaurantOrderingActivity extends AppCompatActivity {
-    private String targetIp;
-    private int targetPort;
-    private SocketManager socketManager;
-    private AppCompatImageButton ibtnOrderingBack;
-    private AppCompatTextView tvOrderingCompanyName;
-    private AppCompatTextView tvOrderingCompanyDesc;
-    private AppCompatTextView tvOrderingCompanyAddress;
+    private AppCompatImageButton ibtnRestaurantOrderingBack;
+    private AppCompatTextView tvRestaurantOrderingCompanyName;
+    private ChipGroup cgRestaurantOrderingCategoryList;
+    private RecyclerView rvRestaurantOrderingProductList;
+    private LinearLayoutCompat llRestaurantOrderingSelected;
+    private AppCompatTextView tvRestaurantOrderingPayAmount;
+    private AppCompatTextView tvRestaurantOrderingSelectedCount;
+    private LinearLayoutCompat llRestaurantOrderingPay;
+    private JSONObject companyData;
+    private JSONArray productData;
+
+    private RestaurantOrderingAdapter restaurantOrderingAdapter;
+    private RestaurantBascketManager restaurantBascketManager;
+
+    private String tempJson = "{\"message\":{\"company\":{\"name\":\"맥도날드\"},\"category_list\":[{\"category\":\"테스트카테고리\",\"product_list\":[{\"name\":\"상품1\",\"price\":500,\"desc\":\"상품1입니다\",\"image\":\"imgdata\"},{\"name\":\"상품2\",\"price\":700,\"desc\":\"상품2입니다\",\"image\":\"imgdata\"},{\"name\":\"상품3\",\"price\":900,\"desc\":\"상품3입니다\",\"image\":\"imgdata\"},{\"name\":\"상품4\",\"price\":1000,\"desc\":\"상품4입니다\",\"image\":\"imgdata\"}],\"set_list\":[{\"name\":\"세트1\",\"price\":3000,\"desc\":\"세트1입니다\",\"image\":\"imgdata\",\"product_list\":[{\"name\":\"서브상품1\",\"price\":500,\"desc\":\"서브상품1입니다\",\"image\":\"imgdata\"},{\"name\":\"서브상품2\",\"price\":500,\"desc\":\"서브상품2입니다\",\"image\":\"imgdata\"},{\"name\":\"서브상품3\",\"price\":500,\"desc\":\"서브상품3입니다\",\"image\":\"imgdata\"},{\"name\":\"서브상품4\",\"price\":500,\"desc\":\"서브상품4입니다\",\"image\":\"imgdata\"}]}]}]}}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,57 +52,78 @@ public class RestaurantOrderingActivity extends AppCompatActivity {
     }
 
     private void init() {
-        ibtnOrderingBack = findViewById(R.id.ibtn_ordering_back);
-        tvOrderingCompanyName = findViewById(R.id.tv_ordering_companyname);
-        tvOrderingCompanyDesc = findViewById(R.id.tv_ordering_companydesc);
-        tvOrderingCompanyAddress = findViewById(R.id.tv_ordering_companyaddress);
+        ibtnRestaurantOrderingBack = findViewById(R.id.ibtn_restaurantordering_back);
+        tvRestaurantOrderingCompanyName = findViewById(R.id.tv_restaurantordering_companyname);
+        cgRestaurantOrderingCategoryList = findViewById(R.id.cg_restaurantordering_categorylist);
+        rvRestaurantOrderingProductList = findViewById(R.id.rv_restaurantordering_productlist);
+        llRestaurantOrderingSelected = findViewById(R.id.ll_restaurantordering_selected);
+        tvRestaurantOrderingPayAmount = findViewById(R.id.tv_restaurantordering_payamount);
+        tvRestaurantOrderingSelectedCount = findViewById(R.id.tv_restaurantordering_selectedcount);
+        llRestaurantOrderingPay = findViewById(R.id.ll_restaurantordering_pay);
 
-        Intent intent = getIntent();
-        targetIp = intent.getStringExtra("targetIp");
-        targetPort = intent.getIntExtra("targetPort", 0);
+        tvRestaurantOrderingPayAmount.setText("0");
+        tvRestaurantOrderingSelectedCount.setText("0");
 
-        socketManager = new SocketManager();
-        socketManager.setSocket(targetIp, targetPort);
+        Intent loadData = getIntent();
 
-        new Thread(() -> {
-            if (socketManager.connect(2000)) {
-                JSONObject sendData = new JSONObject();
+        try {
+            //companyData = new JSONObject(loadData.getStringExtra("companyData"));
+            //productData = new JSONArray(loadData.getStringExtra("productData"));
+            companyData = new JSONObject(tempJson).getJSONObject("message").getJSONObject("company");
+            productData = new JSONObject(tempJson).getJSONObject("message").getJSONArray("category_list");
+            tvRestaurantOrderingCompanyName.setText(companyData.getString("name"));
+        } catch (Exception e) {
+            Toast.makeText(RestaurantOrderingActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
 
-                try {
-                    sendData.accumulate("request_code", Constants.Network.Request.STORE_PRODUCT_INFO);
-                    socketManager.send(sendData.toString());
-
-                    String recvData = socketManager.recv();
-
-                    if (recvData == null) {
-                        Toast.makeText(RestaurantOrderingActivity.this, "매장 연결 중 문제가 발생했습니다", Toast.LENGTH_SHORT).show();
-                    } else {
-                        JSONObject data = new JSONObject(recvData);
-                        String responseCode = data.getString("response_code");
-
-                        if (responseCode.equals(Constants.Network.Response.SERVER_NOT_ONLINE)) {
-                            Toast.makeText(RestaurantOrderingActivity.this, "매장 연결 중 문제가 발생했습니다", Toast.LENGTH_SHORT).show();
-                        } else {
-                            JSONObject companyData = data.getJSONObject("message").getJSONObject("company");
-                            JSONArray productData = data.getJSONObject("message").getJSONArray("product_list");
-
-                            tvOrderingCompanyName.setText(companyData.getString("name"));
-                            tvOrderingCompanyDesc.setText(companyData.getString("desc"));
-                            tvOrderingCompanyAddress.setText(companyData.getString("address"));
-
-                        }
-                    }
-                } catch (JSONException e) {
-                    runOnUiThread(() -> {
-                        Toast.makeText(RestaurantOrderingActivity.this, "매장 연결 중 문제가 발생했습니다: " + e.toString(), Toast.LENGTH_SHORT).show();
-                    });
-                }
-            } else {
-                runOnUiThread(() -> {
-                    Toast.makeText(RestaurantOrderingActivity.this, "매장 연결 중 문제가 발생했습니다", Toast.LENGTH_SHORT).show();
-                });
+        // 카테고리를 chipgroup에 추가
+        for (int loop = 0; loop < productData.length(); loop++) {
+            Chip chip = new Chip(RestaurantOrderingActivity.this);
+            try {
+                chip.setText(productData.getJSONObject(loop).getString("category"));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            chip.setChipDrawable(ChipDrawable.createFromAttributes(RestaurantOrderingActivity.this, null, 0, R.style.Widget_MaterialComponents_Chip_Choice));
+            cgRestaurantOrderingCategoryList.addView(chip);
+        }
+
+        restaurantOrderingAdapter = new RestaurantOrderingAdapter();
+        restaurantOrderingAdapter.setJson(productData);
+        rvRestaurantOrderingProductList.setLayoutManager(new GridLayoutManager(RestaurantOrderingActivity.this, 2, GridLayoutManager.VERTICAL, false));
+        rvRestaurantOrderingProductList.setAdapter(restaurantOrderingAdapter);
+        restaurantBascketManager = new RestaurantBascketManager();
+
+        // 뒤로가기 버튼이 눌렸을 경우
+        ibtnRestaurantOrderingBack.setOnClickListener(view -> {
             finish();
-        }).start();
+        });
+
+        // 리스트 아이템이 눌렸을 경우
+        restaurantOrderingAdapter.setOnItemClickListener((view, position) -> {
+            RestaurantOrderingProductItem restaurantOrderingItem = restaurantOrderingAdapter.getItem(position);
+
+            if (restaurantOrderingItem.getType() == Constants.ItemType.PRODUCT) {
+                new SelectProductDialog(RestaurantOrderingActivity.this, restaurantOrderingItem, (count) -> {
+                    int currentPayAmount = Integer.parseInt(tvRestaurantOrderingPayAmount.getText().toString().replace(",", ""));
+                    int currentSelectedCount = Integer.parseInt(tvRestaurantOrderingSelectedCount.getText().toString());
+
+                    restaurantBascketManager.addItem(restaurantOrderingItem, count);
+                    tvRestaurantOrderingPayAmount.setText(new DecimalFormat("###,###").format(currentPayAmount + restaurantOrderingItem.getPrice() * count));
+                    tvRestaurantOrderingSelectedCount.setText(String.valueOf(currentSelectedCount + count));
+                }).show();
+            }
+        });
+
+        // 선택정보창 버튼이 눌렸을 경우
+        llRestaurantOrderingSelected.setOnClickListener(view -> {
+
+        });
+
+        // 결제 버튼이 눌렸을 경우
+        llRestaurantOrderingPay.setOnClickListener(view -> {
+            Intent intent = new Intent(RestaurantOrderingActivity.this, PayActivity.class);
+            startActivity(intent);
+        });
     }
 }
