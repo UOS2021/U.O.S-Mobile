0081e87a257398357c34999920e0dd975397aac6
Sohn Young Jin <sonkim1001@naver.com>
2021-08-17 17:43:22 +0900

    Add soft keyboard done button listener and show total pay amount.

--- app/src/main/java/com/uof/uof_mobile/dialog/SelectProductDialog.java
+++ app/src/main/java/com/uof/uof_mobile/dialog/SelectProductDialog.java
@@ -5,16 +5,19 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.uof.uof_mobile.R;
+import com.uof.uof_mobile.manager.UsefulFuncManager;
 import com.uof.uof_mobile.recyclerview.RestaurantOrderingProductItem;
 
 public class SelectProductDialog extends Dialog {
@@ -26,7 +29,8 @@ public class SelectProductDialog extends Dialog {
     private AppCompatImageButton ibtnDlgSelectProductCountDown;
     private TextInputLayout tilDlgSelectProductProductCount;
     private AppCompatImageButton ibtnDlgSelectProductCountUp;
-    private AppCompatButton btnDlgSelectProductAdd;
+    private AppCompatTextView tvDlgSelectProductPayAmount;
+    private AppCompatTextView tvDlgSelectProductAdd;
     private final SelectProductDialogListener selectProductDialogListener;
     private RestaurantOrderingProductItem restaurantOrderingProduct;
 
@@ -56,15 +60,17 @@ public class SelectProductDialog extends Dialog {
         tvDlgSelectProductName = findViewById(R.id.tv_dlgselectproduct_name);
         ivDlgSelectProductImage = findViewById(R.id.iv_dlgselectproduct_image);
         tvDlgSelectProductDesc = findViewById(R.id.tv_dlgselectproduct_desc);
-        btnDlgSelectProductAdd = findViewById(R.id.btn_dlgselectproduct_add);
         ibtnDlgSelectProductCountDown = findViewById(R.id.ibtn_dlgselectproduct_countdown);
-        tilDlgSelectProductProductCount = findViewById(R.id.til_dlgselectproduct_productcount);
+        tilDlgSelectProductProductCount = findViewById(R.id.til_dlgselectproduct_count);
         ibtnDlgSelectProductCountUp = findViewById(R.id.ibtn_dlgselectproduct_countup);
+        tvDlgSelectProductPayAmount = findViewById(R.id.tv_dlgselectproduct_payamount);
+        tvDlgSelectProductAdd = findViewById(R.id.tv_dlgselectproduct_add);
 
         tvDlgSelectProductName.setText(restaurantOrderingProduct.getName());
         ivDlgSelectProductImage.setImageBitmap(restaurantOrderingProduct.getImage());
         tvDlgSelectProductDesc.setText(restaurantOrderingProduct.getDesc());
-        tilDlgSelectProductProductCount.getEditText().setText("1");
+        tilDlgSelectProductProductCount.getEditText().setText("0");
+        tvDlgSelectProductPayAmount.setText("0");
 
         // 다이얼로그 종료 버튼 클릭 시
         ibtnDlgSelectProductClose.setOnClickListener(view -> {
@@ -74,8 +80,9 @@ public class SelectProductDialog extends Dialog {
         // 상품 개수 감소 버튼 클릭 시
         ibtnDlgSelectProductCountDown.setOnClickListener(view -> {
             int currentCount = Integer.valueOf(tilDlgSelectProductProductCount.getEditText().getText().toString());
-            if (currentCount > 1) {
+            if (currentCount > 0) {
                 tilDlgSelectProductProductCount.getEditText().setText(String.valueOf(currentCount - 1));
+                tvDlgSelectProductPayAmount.setText(UsefulFuncManager.convertToCommaPattern(restaurantOrderingProduct.getPrice() * Integer.valueOf(tilDlgSelectProductProductCount.getEditText().getText().toString())));
             }
         });
 
@@ -94,23 +101,33 @@ public class SelectProductDialog extends Dialog {
             @Override
             public void afterTextChanged(Editable editable) {
                 if (editable.toString().equals("")) {
-                    tilDlgSelectProductProductCount.getEditText().setText("1");
+                    tilDlgSelectProductProductCount.getEditText().setText("0");
                 } else {
                     int currentCount = Integer.valueOf(editable.toString());
-                    if (currentCount < 1) {
-                        tilDlgSelectProductProductCount.getEditText().setText("1");
+                    if (currentCount < 0) {
+                        tilDlgSelectProductProductCount.getEditText().setText("0");
                     }
                 }
             }
         });
 
+        // 키보드에서 Done(완료) 버튼 누를 시
+        tilDlgSelectProductProductCount.getEditText().setOnEditorActionListener((v, actionId, event) -> {
+            if(actionId== EditorInfo.IME_ACTION_DONE){
+                tvDlgSelectProductPayAmount.setText(UsefulFuncManager.convertToCommaPattern(restaurantOrderingProduct.getPrice() * Integer.valueOf(tilDlgSelectProductProductCount.getEditText().getText().toString())));
+                tilDlgSelectProductProductCount.getEditText().clearFocus();
+            }
+            return false;
+        });
+
         // 상품 개수 증가 버튼 클릭 시
         ibtnDlgSelectProductCountUp.setOnClickListener(view -> {
             tilDlgSelectProductProductCount.getEditText().setText(String.valueOf(Integer.valueOf(tilDlgSelectProductProductCount.getEditText().getText().toString()) + 1));
+            tvDlgSelectProductPayAmount.setText(UsefulFuncManager.convertToCommaPattern(restaurantOrderingProduct.getPrice() * Integer.valueOf(tilDlgSelectProductProductCount.getEditText().getText().toString())));
         });
 
         // 상품 추가 버튼 클릭 시
-        btnDlgSelectProductAdd.setOnClickListener(view -> {
+        tvDlgSelectProductAdd.setOnClickListener(view -> {
             this.selectProductDialogListener.onAddProductClicked(Integer.parseInt(tilDlgSelectProductProductCount.getEditText().getText().toString()));
             dismiss();
         });
