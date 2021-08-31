7b87a33f174dcfded078bcc3a66cb9ead218a7c9
Sohn Young Jin <sonkim1001@naver.com>
2021-08-31 18:54:48 +0900

    Classification of implementation functions according to user type.

--- app/src/main/java/com/uof/uof_mobile/activity/QRRecognitionActivity.java
+++ app/src/main/java/com/uof/uof_mobile/activity/QRRecognitionActivity.java
@@ -49,48 +49,63 @@ public class QRRecognitionActivity extends AppCompatActivity {
                 if (Global.socketManager.connect(2000)) {
                     JSONObject sendData = new JSONObject();
 
-                    try {
-                        sendData.accumulate("request_code", Global.Network.Request.STORE_PRODUCT_INFO);
-                        Global.socketManager.send(sendData.toString());
+                    if(Global.User.type.equals("customer")) {
+                        try {
+                            sendData.accumulate("request_code", Global.Network.Request.STORE_PRODUCT_INFO);
+                            Global.socketManager.send(sendData.toString());
 
-                        String strRecvData = Global.socketManager.recv();
+                            String strRecvData = Global.socketManager.recv();
 
-                        if (strRecvData == null) {
-                            // 수신 데이터가 없을 경우
-                            Toast.makeText(QRRecognitionActivity.this, "매장 연결 중 문제가 발생했습니다", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // 수신 데이터가 있을 경우
-                            JSONObject recvData = new JSONObject(strRecvData);
-                            Global.socketManager.disconnect();
-
-                            JSONObject companyData = recvData.getJSONObject("message").getJSONObject("company");
-                            JSONArray productData = recvData.getJSONObject("message").getJSONArray("category_list");
-
-                            String companyType = companyData.getString("type");
-
-                            if (companyType.equals("restaurant") || companyType.equals("pc")) {
-                                // 회사 종류 - 음식점
-                                Intent intent = new Intent(QRRecognitionActivity.this, OrderingActivity.class);
-                                intent.putExtra("companyData", companyData.toString());
-                                intent.putExtra("productData", productData.toString());
-                                startActivity(intent);
-                            } else if (companyType.equals("movie")) {
-                                // 회사 종류 - 영화관
-                                Intent intent = new Intent(QRRecognitionActivity.this, MovieOrderingActivity.class);
-                                intent.putExtra("companyData", companyData.toString());
-                                intent.putExtra("productData", productData.toString());
-                                startActivity(intent);
+                            if (strRecvData == null) {
+                                // 수신 데이터가 없을 경우
+                                Toast.makeText(QRRecognitionActivity.this, "매장 연결 중 문제가 발생했습니다", Toast.LENGTH_SHORT).show();
                             } else {
-                                runOnUiThread(() -> {
-                                    Toast.makeText(QRRecognitionActivity.this, "유효하지 않은 매장 정보입니다", Toast.LENGTH_SHORT).show();
-                                });
+                                // 수신 데이터가 있을 경우
+                                JSONObject recvData = new JSONObject(strRecvData);
+
+                                JSONObject companyData = recvData.getJSONObject("message").getJSONObject("company");
+                                JSONArray productData = recvData.getJSONObject("message").getJSONArray("category_list");
+
+                                String companyType = companyData.getString("type");
+
+                                if (companyType.equals("restaurant") || companyType.equals("pc")) {
+                                    // 회사 종류 - 음식점 또는 PC방
+                                    Intent intent = new Intent(QRRecognitionActivity.this, OrderingActivity.class);
+                                    intent.putExtra("companyData", companyData.toString());
+                                    intent.putExtra("productData", productData.toString());
+                                    startActivity(intent);
+                                } else if (companyType.equals("movie")) {
+                                    // 회사 종류 - 영화관
+                                    Intent intent = new Intent(QRRecognitionActivity.this, MovieOrderingActivity.class);
+                                    intent.putExtra("companyData", companyData.toString());
+                                    intent.putExtra("productData", productData.toString());
+                                    startActivity(intent);
+                                } else {
+                                    runOnUiThread(() -> {
+                                        Toast.makeText(QRRecognitionActivity.this, "유효하지 않은 매장 정보입니다", Toast.LENGTH_SHORT).show();
+                                    });
+                                }
                             }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            runOnUiThread(() -> {
+                                Toast.makeText(QRRecognitionActivity.this, "매장 연결 중 문제가 발생했습니다: " + e.toString(), Toast.LENGTH_SHORT).show();
+                                Global.socketManager.disconnect();
+                            });
                         }
-                    } catch (Exception e) {
-                        runOnUiThread(() -> {
-                            Toast.makeText(QRRecognitionActivity.this, "매장 연결 중 문제가 발생했습니다: " + e.toString(), Toast.LENGTH_SHORT).show();
-                            Global.socketManager.disconnect();
-                        });
+                    } else if(Global.User.type.equals("uofpartner")){
+                        try {
+                            sendData.accumulate("request_code", Global.Network.Request.STORE_PRODUCT_INFO);
+                            Global.socketManager.send(sendData.toString());
+
+                            String strRecvData = Global.socketManager.recv();
+                        } catch (Exception e){
+                            e.printStackTrace();
+                            runOnUiThread(() -> {
+                                Toast.makeText(QRRecognitionActivity.this, "매장 연결 중 문제가 발생했습니다: " + e.toString(), Toast.LENGTH_SHORT).show();
+                                Global.socketManager.disconnect();
+                            });
+                        }
                     }
                 } else {
                     runOnUiThread(() -> {
