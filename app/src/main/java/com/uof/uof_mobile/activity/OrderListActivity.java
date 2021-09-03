a4df472a72db8f07e796c337f47c65f0815575b5
Sohn Young Jin <sonkim1001@naver.com>
2021-09-03 18:11:52 +0900

    Update OrderListActivity
    Add show waiting order count and finished order count to screen.
    Show past orders in RecyclerView.

--- app/src/main/java/com/uof/uof_mobile/activity/OrderListActivity.java
+++ app/src/main/java/com/uof/uof_mobile/activity/OrderListActivity.java
@@ -1,29 +1,38 @@
 package com.uof.uof_mobile.activity;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.uof.uof_mobile.R;
 import com.uof.uof_mobile.adapter.OrderListAdapter;
+import com.uof.uof_mobile.manager.HttpManager;
+import com.uof.uof_mobile.manager.SQLiteManager;
+import com.uof.uof_mobile.other.Global;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class OrderListActivity extends AppCompatActivity {
-    private final String tempJson =
-            "{\"response_code\": \"0011\", \"message\": " +
-                    "[{\"date\": \"7/28\", " +
-                    "\"companyname\": \"버거킹\", " +
-                    "\"orderlist\": [{\"name\": \"와퍼\", \"count\": 3}, {\"name\": \"통새우와퍼\", \"count\": 2}]," +
-                    "\"price\": 23000}]}";
-    private AppCompatImageButton ibtnOrderlistBack;
+    private AppCompatImageButton ibtnOrderListBack;
+    private AppCompatTextView tvOrderListWaitingOrderCount;
+    private AppCompatTextView tvOrderListDoneOrderCount;
+    private SwipeRefreshLayout srlOrderList;
+    private RecyclerView rvOrderList;
+    private ProgressBar pbOrderList;
+    private AppCompatTextView tvOrderListNoOrderList;
     private OrderListAdapter orderListAdapter;
-    private JSONArray orderlist;
+    private boolean isFirstLoad = true;
+    private SQLiteManager sqLiteManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +43,195 @@ public class OrderListActivity extends AppCompatActivity {
     }
 
     private void init() {
-        ibtnOrderlistBack = findViewById(R.id.ibtn_orderlist_back);
+        ibtnOrderListBack = findViewById(R.id.ibtn_orderlist_back);
+        tvOrderListWaitingOrderCount = findViewById(R.id.tv_orderlist_waitingordercount);
+        tvOrderListDoneOrderCount = findViewById(R.id.tv_orderlist_doneordercount);
+        srlOrderList = findViewById(R.id.srl_orderlist);
+        rvOrderList = findViewById(R.id.rv_orderlist);
+        pbOrderList = findViewById(R.id.pb_orderlist);
+        tvOrderListNoOrderList = findViewById(R.id.tv_orderlist_noorderlist);
 
-        try {
-            orderlist = new JSONObject(tempJson).getJSONArray("message");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(OrderListActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
-        }
+        sqLiteManager = new SQLiteManager(OrderListActivity.this);
 
         orderListAdapter = new OrderListAdapter();
-        orderListAdapter.setJson(orderlist);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_orderlist_orderlistview);
-        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(manager);
-        recyclerView.setAdapter(orderListAdapter);
+        rvOrderList.setLayoutManager(new LinearLayoutManager(OrderListActivity.this, LinearLayoutManager.VERTICAL, false));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(OrderListActivity.this, DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.recyclerview_divider));
+        rvOrderList.addItemDecoration(dividerItemDecoration);
+        rvOrderList.setAdapter(orderListAdapter);
 
+        doUpdateOrderScreen();
 
-        ibtnOrderlistBack.setOnClickListener(view -> {
+        // 뒤로가기 버튼 눌릴 시
+        ibtnOrderListBack.setOnClickListener(view -> {
             finish();
         });
 
+        srlOrderList.setOnRefreshListener(() -> doUpdateOrderScreen());
     }
+
+    public void doUpdateOrderScreen() {
+        new UpdateOrderScreen().start();
+    }
+
+    public class UpdateOrderScreen extends Thread {
+        @Override
+        public void run() {
+            sqLiteManager.openDatabase();
+            int waitingOrderCount = sqLiteManager.getWaitingOrderCount();
+            sqLiteManager.closeDatabase();
+
+            runOnUiThread(() -> {
+                if (isFirstLoad) {
+                    pbOrderList.setVisibility(View.VISIBLE);
+                    isFirstLoad = false;
+                }
+                tvOrderListNoOrderList.setVisibility(View.INVISIBLE);
+            });
+            try {
+                JSONObject sendData = new JSONObject();
+                sendData.put("request_code", Global.Network.Request.ORDER_LIST);
+
+                JSONObject message = new JSONObject();
+                message.accumulate("id", Global.User.id);
+
+                sendData.accumulate("message", message);
+
+                String temp = new HttpManager().execute(new String[]{Global.Network.EXTERNAL_SERVER_URL, sendData.toString()}).get();
+
+                JSONObject recvData = new JSONObject(temp);
+
+                String responseCode = recvData.getString("response_code");
+
+                if (responseCode.equals(Global.Network.Response.ORDER_LIST)) {
+                    // 주문내역 불러오기 성공
+                    runOnUiThread(() -> {
+                        try {
+                            rvOrderList.setVisibility(View.VISIBLE);
+                            orderListAdapter.setJson(recvData.getJSONObject("message").getJSONArray("order_list"));
+                            orderListAdapter.notifyDataSetChanged();
+
+                            ValueAnimator waitingOrderCountAnimator = ValueAnimator.ofInt(0, waitingOrderCount);
+                            waitingOrderCountAnimator.setDuration(2000);
+                            waitingOrderCountAnimator.addUpdateListener(valueAnimator -> tvOrderListWaitingOrderCount.setText(String.valueOf(valueAnimator.getAnimatedValue())));
+
+                            ValueAnimator doneOrderCountAnimator = ValueAnimator.ofInt(0, orderListAdapter.getItemCount() - waitingOrderCount);
+                            doneOrderCountAnimator.setDuration(2000);
+                            doneOrderCountAnimator.addUpdateListener(valueAnimator -> tvOrderListDoneOrderCount.setText(String.valueOf(valueAnimator.getAnimatedValue())));
+
+                            waitingOrderCountAnimator.start();
+                            doneOrderCountAnimator.start();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(OrderListActivity.this, "주문내역을 불러오지 못했습니다", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    runOnUiThread(() -> {
+                        Toast.makeText(OrderListActivity.this, "서버 점검 중입니다", Toast.LENGTH_SHORT).show();
+                    });
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                runOnUiThread(() -> {
+                    Toast.makeText(OrderListActivity.this, "서버 점검 중입니다: " + e.toString(), Toast.LENGTH_SHORT).show();
+                });
+            }
+            runOnUiThread(() -> {
+                pbOrderList.setVisibility(View.GONE);
+
+                if (srlOrderList.isRefreshing()) {
+                    srlOrderList.setRefreshing(false);
+                    srlOrderList.setEnabled(true);
+                }
+                if (orderListAdapter.getItemCount() == 0) {
+                    tvOrderListNoOrderList.setVisibility(View.VISIBLE);
+                } else {
+                    tvOrderListNoOrderList.setVisibility(View.INVISIBLE);
+                }
+            });
+        }
+    }
+
+    private final String sampleData = "{\n" +
+            "    \"response_code\": \"0013\"\n" +
+            "    , \"message\": \n" +
+            "    { \n" +
+            "        \"order_list\": \n" +
+            "        [\n" +
+            "            { \n" +
+            "                \"date\": \"2021-01-01\"\n" +
+            "                , \"company_name\": \"버거킹\"\n" +
+            "                , \"product_list\": \n" +
+            "                [\n" +
+            "                    { \"type\": 0, \"menu\": \"팝콘\", \"submenu\": \"\", \"count\": 1, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 1, \"menu\": \"콜라\", \"submenu\": \"\", \"count\": 2, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"불고기버거세트\", \"submenu\": \"감자튀김(R)&콜라(R)\", \"count\": 1, \"price\": 16000 }\n" +
+            "                    ,{ \"type\": 2, \"menu\": \"분노의질주: 더 익스트림\", \"submenu\": \"A1&A3&A5\", \"count\": 3, \"price\": 5000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"menu\", \"submenu\": \"submenu\", \"count\": 1, \"price\": 3000 }\n" +
+            "                ] \n" +
+            "            }\n" +
+            "            , { \n" +
+            "                \"date\": \"2021-01-01\"\n" +
+            "                , \"company_name\": \"버거킹\"\n" +
+            "                , \"product_list\": \n" +
+            "                [\n" +
+            "                    { \"type\": 0, \"menu\": \"팝콘\", \"submenu\": \"\", \"count\": 1, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 1, \"menu\": \"콜라\", \"submenu\": \"\", \"count\": 2, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"불고기버거세트\", \"submenu\": \"감자튀김(R)&콜라(R)\", \"count\": 1, \"price\": 16000 }\n" +
+            "                    ,{ \"type\": 2, \"menu\": \"분노의질주: 더 익스트림\", \"submenu\": \"A1&A3&A5\", \"count\": 3, \"price\": 5000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"menu\", \"submenu\": \"submenu\", \"count\": 1, \"price\": 3000 }\n" +
+            "                ] \n" +
+            "            }\n" +
+            "            , { \n" +
+            "                \"date\": \"2021-01-01\"\n" +
+            "                , \"company_name\": \"버거킹\"\n" +
+            "                , \"product_list\": \n" +
+            "                [\n" +
+            "                    { \"type\": 0, \"menu\": \"팝콘\", \"submenu\": \"\", \"count\": 1, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 1, \"menu\": \"콜라\", \"submenu\": \"\", \"count\": 2, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"불고기버거세트\", \"submenu\": \"감자튀김(R)&콜라(R)\", \"count\": 1, \"price\": 16000 }\n" +
+            "                    ,{ \"type\": 2, \"menu\": \"분노의질주: 더 익스트림\", \"submenu\": \"A1&A3&A5\", \"count\": 3, \"price\": 5000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"menu\", \"submenu\": \"submenu\", \"count\": 1, \"price\": 3000 }\n" +
+            "                ] \n" +
+            "            }\n" +
+            "            , { \n" +
+            "                \"date\": \"2021-01-01\"\n" +
+            "                , \"company_name\": \"버거킹\"\n" +
+            "                , \"product_list\": \n" +
+            "                [\n" +
+            "                    { \"type\": 0, \"menu\": \"팝콘\", \"submenu\": \"\", \"count\": 1, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 1, \"menu\": \"콜라\", \"submenu\": \"\", \"count\": 2, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"불고기버거세트\", \"submenu\": \"감자튀김(R)&콜라(R)\", \"count\": 1, \"price\": 16000 }\n" +
+            "                    ,{ \"type\": 2, \"menu\": \"분노의질주: 더 익스트림\", \"submenu\": \"A1&A3&A5\", \"count\": 3, \"price\": 5000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"menu\", \"submenu\": \"submenu\", \"count\": 1, \"price\": 3000 }\n" +
+            "                ] \n" +
+            "            }\n" +
+            "            , { \n" +
+            "                \"date\": \"2021-01-01\"\n" +
+            "                , \"company_name\": \"버거킹\"\n" +
+            "                , \"product_list\": \n" +
+            "                [\n" +
+            "                    { \"type\": 0, \"menu\": \"팝콘\", \"submenu\": \"\", \"count\": 1, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 1, \"menu\": \"콜라\", \"submenu\": \"\", \"count\": 2, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"불고기버거세트\", \"submenu\": \"감자튀김(R)&콜라(R)\", \"count\": 1, \"price\": 16000 }\n" +
+            "                    ,{ \"type\": 2, \"menu\": \"분노의질주: 더 익스트림\", \"submenu\": \"A1&A3&A5\", \"count\": 3, \"price\": 5000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"menu\", \"submenu\": \"submenu\", \"count\": 1, \"price\": 3000 }\n" +
+            "                ] \n" +
+            "            }\n" +
+            "            , { \n" +
+            "                \"date\": \"2021-01-01\"\n" +
+            "                , \"company_name\": \"버거킹\"\n" +
+            "                , \"product_list\": \n" +
+            "                [\n" +
+            "                    { \"type\": 0, \"menu\": \"팝콘\", \"submenu\": \"\", \"count\": 1, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 1, \"menu\": \"콜라\", \"submenu\": \"\", \"count\": 2, \"price\": 1000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"불고기버거세트\", \"submenu\": \"감자튀김(R)&콜라(R)\", \"count\": 1, \"price\": 16000 }\n" +
+            "                    ,{ \"type\": 2, \"menu\": \"분노의질주: 더 익스트림\", \"submenu\": \"A1&A3&A5\", \"count\": 3, \"price\": 5000 }\n" +
+            "                    ,{ \"type\": 0, \"menu\": \"menu\", \"submenu\": \"submenu\", \"count\": 1, \"price\": 3000 }\n" +
+            "                ] \n" +
+            "            }\n" +
+            "        ] \n" +
+            "    } \n" +
+            "}";
 }
