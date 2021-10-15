8c2b8c33f9fd1beddd57ad7e2b98c4adabd16b8f
ILoveGameCoding <sonkim1001@naver.com>
2021-10-15 17:57:15 +0900

    Update LoginActivity
    Separate login logic for customers and UOS partners.

--- app/src/main/java/com/uos/uos_mobile/activity/LoginActivity.java
+++ app/src/main/java/com/uos/uos_mobile/activity/LoginActivity.java
@@ -11,7 +11,6 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import com.google.android.material.textfield.TextInputLayout;
-
 import com.uos.uos_mobile.dialog.RegisterTypeDialog;
 import com.uos.uos_mobile.manager.HttpManager;
 import com.uos.uos_mobile.manager.SharedPreferencesManager;
@@ -120,16 +119,16 @@ public class LoginActivity extends UosActivity {
 
         /* 회원가입 버튼(TextView)이 눌렸을 경우 */
         tvLoginRegister.setOnClickListener(view -> {
-            
+
             /* 회원가입 다이얼로그 호출 */
-            
+
             new RegisterTypeDialog(LoginActivity.this, true, true,
                     new RegisterTypeDialog.RegisterTypeDialogListener() {
                         @Override
                         public void onCustomerClick() {
-                            
+
                             /* 회원가입 종류가 일반고객일 경우 */
-                            
+
                             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                             intent.putExtra("RegisterType", 0);
                             startActivity(intent);
@@ -137,9 +136,9 @@ public class LoginActivity extends UosActivity {
 
                         @Override
                         public void onUosPartnerClick() {
-                            
+
                             /* 회원가입 종류가 U.O.S 파트너일 경우 */
-                            
+
                             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                             intent.putExtra("RegisterType", 1);
                             startActivity(intent);
@@ -153,13 +152,13 @@ public class LoginActivity extends UosActivity {
         });
 
         SharedPreferencesManager.open(LoginActivity.this, Global.SharedPreference.APP_DATA);
-        if ((Boolean)SharedPreferencesManager.load(Global.SharedPreference.IS_LOGINED, false)) {
+        if ((Boolean) SharedPreferencesManager.load(Global.SharedPreference.IS_LOGINED, false)) {
 
             /* 자동 로그인이 활성화 되어있을 경우 */
 
-            tilLoginId.getEditText().setText((String)SharedPreferencesManager.load(Global.SharedPreference.USER_ID, ""));
-            tilLoginPw.getEditText().setText((String)SharedPreferencesManager.load(Global.SharedPreference.USER_PW, ""));
-            cbLoginIsPartner.setChecked(((String)SharedPreferencesManager.load(Global.SharedPreference.USER_TYPE, "")).equals("uospartner"));
+            tilLoginId.getEditText().setText((String) SharedPreferencesManager.load(Global.SharedPreference.USER_ID, ""));
+            tilLoginPw.getEditText().setText((String) SharedPreferencesManager.load(Global.SharedPreference.USER_PW, ""));
+            cbLoginIsPartner.setChecked(SharedPreferencesManager.load(Global.SharedPreference.USER_TYPE, "").toString().equals("uospartner"));
 
             login();
         }
@@ -172,7 +171,7 @@ public class LoginActivity extends UosActivity {
      */
     private void login() {
         btnLoginLogin.setEnabled(false);
-        
+
         try {
 
             /* 외부서버로 송신할 데이터 설정 */
@@ -183,6 +182,7 @@ public class LoginActivity extends UosActivity {
                 message.accumulate("type", "uospartner");
             } else {
                 message.accumulate("type", "customer");
+                message.accumulate("fcm_token", Global.Firebase.FCM_TOKEN);
             }
 
             JSONObject sendData = new JSONObject();
@@ -193,25 +193,52 @@ public class LoginActivity extends UosActivity {
             JSONObject recvData = new JSONObject(new HttpManager().execute(new String[]{Global.Network.EXTERNAL_SERVER_URL, String.valueOf(HttpManager.DEFAULT_CONNECTION_TIMEOUT), String.valueOf(HttpManager.DEFAULT_READ_TIMEOUT), sendData.toString()}).get());
             String responseCode = recvData.getString("response_code");
 
-            if (responseCode.equals(Global.Network.Response.LOGIN_SUCCESS)) {
+            if (responseCode.equals(Global.Network.Response.LOGIN_SUCCESS_CUSTOMER)) {
 
-                /* 로그인 성공 시 */
+                /* 일반고객 로그인 성공 시 */
 
                 JSONObject userData = recvData.getJSONObject("message");
                 Global.User.id = tilLoginId.getEditText().getText().toString();
                 Global.User.name = userData.getString("name");
                 Global.User.phone = userData.getString("phone");
-                Global.User.type = userData.getString("type");
+                Global.User.type = "customer";
 
-                if(Global.User.type.equals("uospartner")){
-                    
-                    /* 만약 로그인한 사용자가 U.O.S 파트너일 경우 수신 데이터에서 매장명과 QR코드 추출 */
-                    
-                    Global.User.companyName = userData.getString("company_name");
-                    SharedPreferencesManager.open(LoginActivity.this, Global.SharedPreference.APP_DATA);
-                    SharedPreferencesManager.save(Global.SharedPreference.QR_IMAGE, recvData.getJSONObject("message").getString("qr_image"));
-                    SharedPreferencesManager.close();
+                /* 로그인한 사용자의 데이터를 Global.USER에 저장 */
+                SharedPreferencesManager.open(LoginActivity.this, Global.SharedPreference.APP_DATA);
+                SharedPreferencesManager.save(Global.SharedPreference.USER_ID, Global.User.id);
+                SharedPreferencesManager.save(Global.SharedPreference.USER_PW, tilLoginPw.getEditText().getText().toString());
+                SharedPreferencesManager.save(Global.SharedPreference.USER_TYPE, Global.User.type);
+                SharedPreferencesManager.save(Global.SharedPreference.IS_LOGINED, true);
+                SharedPreferencesManager.close();
+
+                Intent loginActivityIntent = getIntent();
+                Intent intent = new Intent(LoginActivity.this, LobbyActivity.class);
+                if (loginActivityIntent.getStringExtra("uosPartnerId") != null) {
+
+                    /* QR코드를 통해 앱을 실행했을 경우 */
+
+                    intent.putExtra("uosPartnerId", loginActivityIntent.getStringExtra("uosPartnerId"));
+                } else if (loginActivityIntent.getStringExtra("orderCode") != null) {
+
+                    /* Notofication을 통해 앱을 실행했을 경우 */
+
+                    intent.putExtra("orderCode", loginActivityIntent.getStringExtra("orderCode"));
                 }
+                startActivity(intent);
+                finish();
+            } else if (responseCode.equals(Global.Network.Response.LOGIN_SUCCESS_UOSPARTNER)) {
+
+                /* U.O.S 파트너 로그인 성공 시 */
+
+                JSONObject userData = recvData.getJSONObject("message");
+                Global.User.id = tilLoginId.getEditText().getText().toString();
+                Global.User.name = userData.getString("name");
+                Global.User.phone = userData.getString("phone");
+                Global.User.type = "customer";
+                Global.User.companyName = userData.getString("company_name");
+                SharedPreferencesManager.open(LoginActivity.this, Global.SharedPreference.APP_DATA);
+                SharedPreferencesManager.save(Global.SharedPreference.QR_IMAGE, recvData.getJSONObject("message").getString("qr_img"));
+                SharedPreferencesManager.close();
 
                 /* 로그인한 사용자의 데이터를 Global.USER에 저장 */
                 SharedPreferencesManager.open(LoginActivity.this, Global.SharedPreference.APP_DATA);
@@ -223,43 +250,26 @@ public class LoginActivity extends UosActivity {
 
                 Intent loginActivityIntent = getIntent();
 
-                if (cbLoginIsPartner.isChecked()) {
+                if (loginActivityIntent.getStringExtra("uosPartnerId") != null) {
 
-                    /* U.O.S 파트너가 로그인일 경우 */
+                    /* QR코드를 통해 앱에 접속했을 경우 */
 
-                    if (loginActivityIntent.getStringExtra("uosPartnerId") != null) {
+                    Toast.makeText(LoginActivity.this, "UOS 파트너는 매장 상품 구매가 불가능합니다", Toast.LENGTH_SHORT).show();
+                } else if (loginActivityIntent.getStringExtra("orderCode") != null) {
 
-                        /* QR코드를 통해 앱에 접속했을 경우 */
+                    /* Notofication을 통해 앱을 실행했을 경우 */
 
-                        Toast.makeText(LoginActivity.this, "UOS 파트너는 매장 상품 구매가 불가능합니다", Toast.LENGTH_SHORT).show();
-                    }
-                    startActivity(new Intent(LoginActivity.this, OwnerLobbyActivity.class));
-                } else {
-
-                    /* 일반고객이 로그인할 경우 */
-
-                    Intent intent = new Intent(LoginActivity.this, LobbyActivity.class);
-                    if (loginActivityIntent.getStringExtra("uosPartnerId") != null) {
-
-                        /* QR코드를 통해 앱을 실행했을 경우 */
-
-                        intent.putExtra("uosPartnerId", loginActivityIntent.getStringExtra("uosPartnerId"));
-                    }else if (loginActivityIntent.getStringExtra("orderCode") != null) {
-                        
-                        /* Notofication을 통해 앱을 실행했을 경우 */
-                        
-                        intent.putExtra("orderCode", loginActivityIntent.getStringExtra("orderCode"));
-                    }
-                    startActivity(intent);
+                    Toast.makeText(LoginActivity.this, "UOS 파트너는 매장 상품 구매가 불가능합니다", Toast.LENGTH_SHORT).show();
                 }
+                startActivity(new Intent(LoginActivity.this, OwnerLobbyActivity.class));
                 finish();
-            } else if (responseCode.equals(Global.Network.Response.LOGIN_FAILED_ID_NOT_EXIST)) {
+            } else if (responseCode.equals(Global.Network.Response.LOGIN_FAIL_ID_NOT_EXIST)) {
 
                 /* 로그인 실패 - 아이디 없음 */
 
                 tilLoginId.setError("아이디가 존재하지 않습니다");
                 tilLoginId.setErrorEnabled(true);
-            } else if (responseCode.equals(Global.Network.Response.LOGIN_CHECKPW_FAILED_PW_NOT_CORRECT)) {
+            } else if (responseCode.equals(Global.Network.Response.LOGIN_FAIL_PW_NOT_CORRECT)) {
 
                 /* 로그인 실패 - 비밀번호 틀림 */
 
@@ -274,7 +284,7 @@ public class LoginActivity extends UosActivity {
 
                 /* 로그인 실패 - 기타 오류 */
 
-                Toast.makeText(LoginActivity.this, "로그인 실패: " + recvData.getString("message"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "로그인 시도 중 오류가 발생했습니다", Toast.LENGTH_SHORT).show();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -299,11 +309,11 @@ public class LoginActivity extends UosActivity {
         tilLoginPw.setErrorEnabled(false);
         tilLoginId.setError(null);
         tilLoginPw.setError(null);
-        
+
         if (tilLoginId.getEditText().getText().toString().length() > 0 && tilLoginPw.getEditText().getText().toString().length() > 0) {
-            
+
             /* 로그인 버튼 활성화 조건을 만족시켰을 경우 */
-        
+
             btnLoginLogin.setEnabled(true);
             btnLoginLogin.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.color_primary));
         } else {
