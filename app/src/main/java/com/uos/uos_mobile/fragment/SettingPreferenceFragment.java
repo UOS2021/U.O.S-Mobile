beb6099c5f12f4a9057d06fda70d88c219c9772a
ILoveGameCoding <sonkim1001@naver.com>
2021-10-15 18:27:06 +0900

    Update SettingPreferenceFragment
    Remove unused code.
    Add http connection in logout logic.

--- app/src/main/java/com/uos/uos_mobile/fragment/SettingPreferenceFragment.java
+++ app/src/main/java/com/uos/uos_mobile/fragment/SettingPreferenceFragment.java
@@ -2,27 +2,29 @@ package com.uos.uos_mobile.fragment;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
-
+import android.widget.Toast;
 
 import com.uos.uos_mobile.activity.LoginActivity;
 import com.uos.uos_mobile.activity.UosActivity;
 import com.uos.uos_mobile.dialog.ChangePhoneDialog;
 import com.uos.uos_mobile.dialog.ChangePwDialog;
 import com.uos.uos_mobile.dialog.WithdrawalDialog;
+import com.uos.uos_mobile.manager.HttpManager;
 import com.uos.uos_mobile.manager.SharedPreferencesManager;
 import com.uos.uos_mobile.other.Global;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 
 public class SettingPreferenceFragment extends PreferenceFragment {
     private Context context;
-    private SharedPreferences prefs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,38 +32,56 @@ public class SettingPreferenceFragment extends PreferenceFragment {
 
         context = getActivity();
 
-        addPreferencesFromResource(com.uos.uos_mobile.R.xml.settings_preference);
-        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        SharedPreferences.OnSharedPreferenceChangeListener prefsListener = (sharedPreferences, key) -> {
-            if (key.equals("vibration") && sharedPreferences.getBoolean("vibration", false) == true) {
-                Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-                vibrator.vibrate(500);
-            }
-        };
-
         Preference btnChangePw = findPreference(getString(com.uos.uos_mobile.R.string.setting_change_pw));
         Preference btnChangePhone = findPreference(getString(com.uos.uos_mobile.R.string.setting_change_phone));
         Preference btnLogout = findPreference(getString(com.uos.uos_mobile.R.string.setting_logout));
         Preference btnWithdrawal = findPreference(getString(com.uos.uos_mobile.R.string.setting_withdrawal));
-        PreferenceScreen btnAccountSetting = getPreferenceScreen();
 
-        // 비밀번호 변경이 눌렸을 경우
+        /* 비밀번호 변경이 눌렸을 경우 */
         btnChangePw.setOnPreferenceClickListener(preference -> {
             new ChangePwDialog(context, false, true).show();
 
             return false;
         });
 
-        // 전화번호 변경이 눌렸을 경우
+        /* 전화번호 변경이 눌렸을 경우 */
         btnChangePhone.setOnPreferenceClickListener(preference -> {
-            //startActivity(new Intent(context, ChangePhoneActivity.class));
             new ChangePhoneDialog(context, false, true).show();
 
             return false;
         });
 
-        // 로그아웃이 눌렸을 경우
+        /* 로그아웃이 눌렸을 경우 */
         btnLogout.setOnPreferenceClickListener(preference -> {
+            if (Global.User.type.equals("customer")) {
+                try {
+                    JSONObject message = new JSONObject();
+                    message.accumulate("customer_id", Global.User.id);
+
+                    JSONObject sendData = new JSONObject();
+                    sendData.accumulate("request_code", Global.Network.Request.CUSTOMER_LOGOUT);
+                    sendData.accumulate("message", message);
+
+                    JSONObject recvData = new JSONObject(new HttpManager().execute(new String[]{Global.Network.EXTERNAL_SERVER_URL, String.valueOf(HttpManager.DEFAULT_CONNECTION_TIMEOUT), String.valueOf(HttpManager.DEFAULT_READ_TIMEOUT), sendData.toString()}).get());
+                    String responseCode = recvData.getString("response_code");
+
+                    if (responseCode.equals(Global.Network.Response.CUSTOMER_LOGOUT_SUCCESS)) {
+
+                        /* 로그아웃 성공 시 */
+
+                        Toast.makeText(context, "로그아웃 되었습니다", Toast.LENGTH_SHORT).show();
+                    } else {
+                        
+                        /* 로그아웃 실패 시 */
+                        
+                        Toast.makeText(context, "로그아웃 도중 오류가 발생했습니다", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException | InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "로그아웃 도중 오류가 발생했습니다", Toast.LENGTH_SHORT).show();
+                }
+            }
+
             SharedPreferencesManager.open(context, Global.SharedPreference.APP_DATA);
             SharedPreferencesManager.save(Global.SharedPreference.IS_LOGINED, false);
             SharedPreferencesManager.save(Global.SharedPreference.USER_ID, "");
@@ -74,12 +94,11 @@ public class SettingPreferenceFragment extends PreferenceFragment {
             return false;
         });
 
-        // 회원탈퇴가 눌렸을 경우
+        /* 회원탈퇴가 눌렸을 경우 */
         btnWithdrawal.setOnPreferenceClickListener(preference -> {
             new WithdrawalDialog(context, false, true).show();
             return false;
         });
-        prefs.registerOnSharedPreferenceChangeListener(prefsListener);
     }
 
     @Override
