91a8d61370e289f12323cf2abc62c7ac780d3d31
ILoveGameCoding <sonkim1001@naver.com>
2021-10-15 17:17:05 +0900

    Change class name OrderInfoDialog to OrderDetailDialog.

--- app/src/main/java/com/uos/uos_mobile/dialog/OrderDetailDialog.java
+++ app/src/main/java/com/uos/uos_mobile/dialog/OrderDetailDialog.java
@@ -1,12 +1,10 @@
 package com.uos.uos_mobile.dialog;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatDialog;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -17,11 +15,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.uos.uos_mobile.adapter.WaitingOrderInfoAdapter;
 import com.uos.uos_mobile.item.BasketItem;
 import com.uos.uos_mobile.item.OrderListItem;
-import com.uos.uos_mobile.manager.SQLiteManager;
 import com.uos.uos_mobile.manager.UsefulFuncManager;
-import com.uos.uos_mobile.other.Global;
 
-public class OrderInfoDialog extends UosDialog {
+public class OrderDetailDialog extends UosDialog {
     private final Context context;
     private final OrderListItem orderListItem;
     private AppCompatImageButton ibtnDlgOrderInfoClose;
@@ -30,9 +26,8 @@ public class OrderInfoDialog extends UosDialog {
     private AppCompatTextView tvDlgOrderInfoOrderTotalPrice;
     private RecyclerView rvDlgOrderInfo;
     private WaitingOrderInfoAdapter waitingOrderInfoAdapter;
-    private SQLiteManager sqLiteManager;
 
-    public OrderInfoDialog(@NonNull Context context, boolean canceledOnTouchOutside, boolean cancelable, OrderListItem orderListItem) {
+    public OrderDetailDialog(@NonNull Context context, boolean canceledOnTouchOutside, boolean cancelable, OrderListItem orderListItem) {
         super(context, com.uos.uos_mobile.R.style.DialogTheme_FullScreenDialog);
         this.context = context;
         this.orderListItem = orderListItem;
@@ -44,7 +39,7 @@ public class OrderInfoDialog extends UosDialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(com.uos.uos_mobile.R.layout.dialog_orderinfo);
+        setContentView(com.uos.uos_mobile.R.layout.dialog_orderdetail);
         getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
         getWindow().setWindowAnimations(com.uos.uos_mobile.R.style.Anim_FullScreenDialog);
 
@@ -57,8 +52,6 @@ public class OrderInfoDialog extends UosDialog {
         tvDlgOrderInfoOrderTime = findViewById(com.uos.uos_mobile.R.id.tv_dlgorderinfo_ordertime);
         tvDlgOrderInfoOrderTotalPrice = findViewById(com.uos.uos_mobile.R.id.tv_dlgorderinfo_totalprice);
         rvDlgOrderInfo = findViewById(com.uos.uos_mobile.R.id.rv_dlgorderinfo);
-
-        sqLiteManager = new SQLiteManager(context);
 
         tvDlgOrderInfoCompanyName.setText(orderListItem.getCompanyName());
         tvDlgOrderInfoOrderTime.setText(orderListItem.getDate() + " " + orderListItem.getTime());
