e4bd6b40f8f448f8e9e2fcdf43481c1608abca90
ILoveGameCoding <sonkim1001@naver.com>
2021-10-18 01:06:36 +0900

    Rename WaitingOrderDialog to WaitingOrderAcceptDialog.

--- app/src/main/java/com/uos/uos_mobile/dialog/WaitingOrderAcceptDialog.java
+++ app/src/main/java/com/uos/uos_mobile/dialog/WaitingOrderAcceptDialog.java
@@ -8,13 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class WaitingOrderDialog extends UosDialog {
+public class WaitingOrderAcceptDialog extends UosDialog {
     private final Context context;
     private final String companyName;
-    private AppCompatTextView tvDlgWaitingOrderExplain;
-    private ConstraintLayout clDlgWaitingOrderOk;
+    private AppCompatTextView tvDlgWaitingOrderAcceptExplain;
+    private ConstraintLayout clDlgWaitingOrderAcceptOk;
 
-    public WaitingOrderDialog(@NonNull Context context, boolean canceledOnTouchOutside, boolean cancelable, String companyName) {
+    public WaitingOrderAcceptDialog(@NonNull Context context, boolean canceledOnTouchOutside, boolean cancelable, String companyName) {
         super(context, com.uos.uos_mobile.R.style.DialogTheme_FullScreenDialog);
         this.context = context;
         this.companyName = companyName;
@@ -25,7 +25,7 @@ public class WaitingOrderDialog extends UosDialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.uos.uos_mobile.R.layout.dialog_waitingorder);
+        setContentView(com.uos.uos_mobile.R.layout.dialog_waitingorderaccept);
         getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
 
         init();
@@ -35,13 +35,13 @@ public class WaitingOrderDialog extends UosDialog {
      * Dialog 실행 시 최초 실행해야하는 코드 및 변수 초기화를 담당하고 있는 함수.
      */
     protected void init() {
-        tvDlgWaitingOrderExplain = findViewById(com.uos.uos_mobile.R.id.tv_dlgwaitingorder_explain);
-        clDlgWaitingOrderOk = findViewById(com.uos.uos_mobile.R.id.cl_dlgwaitingorder_ok);
+        tvDlgWaitingOrderAcceptExplain = findViewById(com.uos.uos_mobile.R.id.tv_dlgwaitingorderaccept_explain);
+        clDlgWaitingOrderAcceptOk = findViewById(com.uos.uos_mobile.R.id.cl_dlgwaitingorderaccept_ok);
         
-        tvDlgWaitingOrderExplain.setText(companyName + "에서 주문을 확인하고 있습니다\n잠시만 기다려주세요");
+        tvDlgWaitingOrderAcceptExplain.setText(companyName + "에서 주문을 확인하고 있습니다\n잠시만 기다려주세요");
 
         /* 주문취소 버튼이 눌렸을 경우 */
-        clDlgWaitingOrderOk.setOnClickListener(view -> {
+        clDlgWaitingOrderAcceptOk.setOnClickListener(view -> {
             dismiss();
         });
     }
