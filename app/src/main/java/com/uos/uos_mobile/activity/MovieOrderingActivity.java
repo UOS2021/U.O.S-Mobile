65e02b858aba7ce20a21606196cf6c2b10eeb581
ILoveGameCoding <sonkim1001@naver.com>
2021-10-25 02:09:10 +0900

    Update MovieOrderingActivity
    Modify the code to have the same behavior as OrderActivity.

--- app/src/main/java/com/uos/uos_mobile/activity/MovieOrderingActivity.java
+++ app/src/main/java/com/uos/uos_mobile/activity/MovieOrderingActivity.java
@@ -33,9 +33,18 @@ import com.uos.uos_mobile.manager.UsefulFuncManager;
 import com.uos.uos_mobile.other.Global;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * 영화관에서 주문가능한 상품목록을 보여주는 Activity.<br>
+ * xml: activity_movieordering.xml<br><br>
+ *
+ * @author Sohn Young Jin
+ * @since 1.0.0
+ */
 public class MovieOrderingActivity extends UosActivity {
+
     private AppCompatImageButton ibtnMovieOrderingBack;
     private AppCompatTextView tvMovieOrderingCompanyName;
     private AppCompatTextView tvMovieOrderingShowMovie;
@@ -61,14 +70,6 @@ public class MovieOrderingActivity extends UosActivity {
     private BasketManager basketManager;
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    /**
-     * Activity 실행 시 최초 실행해야하는 코드 및 변수 초기화를 담당하고 있는 함수.
-     */
-    @Override
     protected void init() {
         setContentView(com.uos.uos_mobile.R.layout.activity_movieordering);
 
@@ -87,7 +88,7 @@ public class MovieOrderingActivity extends UosActivity {
         tvMovieOrderingProductCount = findViewById(com.uos.uos_mobile.R.id.tv_movieordering_productcount);
         llMovieOrderingPay = findViewById(com.uos.uos_mobile.R.id.ll_movieordering_order);
 
-        // UI 초기 상태 설정
+        /* UI 초기 상태 설정 */
         tvMovieOrderingShowMovie.setEnabled(false);
         tvMovieOrderingShowMovie.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.recyclerview_background));
         llMovieOrderingMovie.setVisibility(View.VISIBLE);
@@ -107,52 +108,51 @@ public class MovieOrderingActivity extends UosActivity {
             categoryData = message.getJSONArray("category_list");
             movieData = message.getJSONArray("movie_list");
             tvMovieOrderingCompanyName.setText(companyData.getString("name"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(MovieOrderingActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
-        }
 
-        // 영화 목록 Adapter 설정
-        movieOrderingAdapter = new MovieOrderingAdapter();
-        movieOrderingAdapter.setJson(movieData);
-        rvMovieOrderingMovieList.setLayoutManager(new LinearLayoutManager(MovieOrderingActivity.this, LinearLayoutManager.VERTICAL, false));
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(MovieOrderingActivity.this, DividerItemDecoration.VERTICAL);
-        dividerItemDecoration.setDrawable(getResources().getDrawable(com.uos.uos_mobile.R.drawable.recyclerview_divider));
-        rvMovieOrderingMovieList.addItemDecoration(dividerItemDecoration);
-        rvMovieOrderingMovieList.setAdapter(movieOrderingAdapter);
+            /* 영화 목록 Adapter 설정 */
+            movieOrderingAdapter = new MovieOrderingAdapter();
+            movieOrderingAdapter.setJson(movieData);
+            rvMovieOrderingMovieList.setLayoutManager(new LinearLayoutManager(MovieOrderingActivity.this, LinearLayoutManager.VERTICAL, false));
+            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(MovieOrderingActivity.this, DividerItemDecoration.VERTICAL);
+            dividerItemDecoration.setDrawable(getResources().getDrawable(com.uos.uos_mobile.R.drawable.recyclerview_divider));
+            rvMovieOrderingMovieList.addItemDecoration(dividerItemDecoration);
+            rvMovieOrderingMovieList.setAdapter(movieOrderingAdapter);
 
-        // 상품 목록 Adapter 설정
-        orderingAdapter = new OrderingAdapter();
-        orderingAdapter.setJson(categoryData);
-        rvMovieOrderingProductList.setLayoutManager(new GridLayoutManager(MovieOrderingActivity.this, 2, GridLayoutManager.VERTICAL, false));
-        rvMovieOrderingProductList.setAdapter(orderingAdapter);
-        basketManager = new BasketManager(tvMovieOrderingCompanyName.getText().toString());
+            /* 상품 목록 Adapter 설정 */
+            orderingAdapter = new OrderingAdapter();
+            orderingAdapter.setJson(categoryData);
+            rvMovieOrderingProductList.setLayoutManager(new GridLayoutManager(MovieOrderingActivity.this, 2, GridLayoutManager.VERTICAL, false));
+            rvMovieOrderingProductList.setAdapter(orderingAdapter);
+            basketManager = new BasketManager();
 
-        updatePriceInfo();
+            updatePriceInfo();
 
-        // 카테고리를 chipgroup에 추가
-        for (int loop = 0; loop < categoryData.length(); loop++) {
-            Chip chip = (Chip) MovieOrderingActivity.this.getLayoutInflater().inflate(com.uos.uos_mobile.R.layout.chip_category, cgMovieOrderingCategoryList, false);
-            try {
+            /* 카테고리를 chipgroup에 추가 */
+            for (int loop = 0; loop < categoryData.length(); loop++) {
+                Chip chip = (Chip) MovieOrderingActivity.this.getLayoutInflater().inflate(com.uos.uos_mobile.R.layout.chip_category, cgMovieOrderingCategoryList, false);
+
                 chip.setText(categoryData.getJSONObject(loop).getString("category"));
                 chip.setOnClickListener(view -> {
                     selectedCategory = chip.getText().toString();
                     orderingAdapter.setSelectedCategory(selectedCategory);
                     rvMovieOrderingProductList.setAdapter(orderingAdapter);
                 });
-            } catch (Exception e) {
-                e.printStackTrace();
+
+                if (loop == 0) {
+                    selectedCategory = chip.getText().toString();
+                    orderingAdapter.setSelectedCategory(selectedCategory);
+                    rvMovieOrderingProductList.setAdapter(orderingAdapter);
+                    chip.setChecked(true);
+                }
+                cgMovieOrderingCategoryList.addView(chip);
             }
-            if (loop == 0) {
-                selectedCategory = chip.getText().toString();
-                orderingAdapter.setSelectedCategory(selectedCategory);
-                rvMovieOrderingProductList.setAdapter(orderingAdapter);
-                chip.setChecked(true);
-            }
-            cgMovieOrderingCategoryList.addView(chip);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(MovieOrderingActivity.this, "상품목록을 불러오는 도중 오류가 발생했습니다", Toast.LENGTH_SHORT).show();
+            finish();
         }
 
-        // 뒤로가기 버튼이 눌렸을 경우
+        /* 뒤로가기 버튼이 눌렸을 경우 */
         ibtnMovieOrderingBack.setOnClickListener(view -> {
             AlertDialog alertDialog = new AlertDialog.Builder(MovieOrderingActivity.this, com.uos.uos_mobile.R.style.AlertDialogTheme)
                     .setTitle("주문 취소")
@@ -170,7 +170,7 @@ public class MovieOrderingActivity extends UosActivity {
             alertDialog.show();
         });
 
-        // 영화 표시 버튼이 눌렸을 때
+        /* 영화 표시 버튼이 눌렸을 경우 */
         tvMovieOrderingShowMovie.setOnClickListener(view -> {
             tvMovieOrderingShowMovie.setEnabled(false);
             tvMovieOrderingShowMovie.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.recyclerview_background));
@@ -181,7 +181,7 @@ public class MovieOrderingActivity extends UosActivity {
             llMovieOrderingFood.setVisibility(View.GONE);
         });
 
-        // 음식 표시 버튼이 눌렸을 때
+        /* 음식 표시 버튼이 눌렸을 경우 */
         tvMovieOrderingShowFood.setOnClickListener(view -> {
             tvMovieOrderingShowMovie.setEnabled(true);
             tvMovieOrderingShowMovie.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.gray));
@@ -192,12 +192,14 @@ public class MovieOrderingActivity extends UosActivity {
             llMovieOrderingFood.setVisibility(View.VISIBLE);
         });
 
-        // 리스트 아이템이 눌렸을 경우
+        /* 리스트 아이템이 눌렸을 경우 */
         orderingAdapter.setOnItemClickListener((view, position) -> {
             OrderingProductItem orderingProductItem = orderingAdapter.getItem(position);
 
             if (orderingProductItem.getType() == Global.ItemType.PRODUCT) {
-                // 선택된 아이템이 단일상품일 경우
+
+                /* 선택된 아이템이 단일상품일 경우 */
+
                 new SelectProductDialog(MovieOrderingActivity.this, orderingProductItem, (orderingItem) -> {
                     if (orderingItem.getCount() >= 1) {
                         basketManager.addItem(orderingItem);
@@ -205,7 +207,9 @@ public class MovieOrderingActivity extends UosActivity {
                     }
                 }).show();
             } else {
-                // 선택된 아이템이 세트상품일 경우
+
+                /* 선택된 아이템이 세트상품일 경우 */
+
                 new SelectSetDialog(MovieOrderingActivity.this, (OrderingSetItem) orderingProductItem, (orderingItem) -> {
                     if (orderingItem.getCount() >= 1) {
                         basketManager.addItem(orderingItem);
@@ -215,49 +219,42 @@ public class MovieOrderingActivity extends UosActivity {
             }
         });
 
-        // 선택정보창 버튼이 눌렸을 경우
+        /* 선택정보창 버튼이 눌렸을 경우 */
         llMovieOrderingSelected.setOnClickListener(view -> {
-            if (basketManager.getOrderCount() == 0) {
-                Toast.makeText(MovieOrderingActivity.this, "장바구니가 비어있습니다", Toast.LENGTH_SHORT).show();
-            } else {
-                BasketDialog basketDialog = new BasketDialog(MovieOrderingActivity.this, uosPartnerId, basketManager);
-                basketDialog.setOnDismissListener(dialogInterface -> {
-                    updatePriceInfo();
-                });
-                basketDialog.show();
-            }
-        });
-
-        // 결제 버튼이 눌렸을 경우
-        llMovieOrderingPay.setOnClickListener(view -> {
-            if (basketManager.getOrderCount() == 0) {
-                Toast.makeText(MovieOrderingActivity.this, "장바구니가 비어있습니다", Toast.LENGTH_SHORT).show();
-            } else {
-                Intent intent = new Intent(MovieOrderingActivity.this, PayActivity.class);
-                intent.putExtra("uosPartnerId", uosPartnerId);
-                intent.putExtra("basketManager", basketManager);
-                startActivity(intent);
-            }
-        });
-
-        // 영화 선택 시
-        movieOrderingAdapter.setOnItemClickListener((view, position) -> new SelectSeatDialog(MovieOrderingActivity.this, movieOrderingAdapter.getItem(position), new SelectSeatDialog.SelectSeatDialogListener() {
-            @Override
-            public void onAddProductClicked(MovieItem updatedMovieItem, BasketItem basketItem) {
-                movieOrderingAdapter.setItem(position, updatedMovieItem.clone());
-                basketItem.setMovieItem(movieOrderingAdapter.getItem(position));
-
-                for (BasketItem basketItem1 : basketManager.getOrderingItemArrayList()) {
-                    if (basketItem1.getMenu().equals(basketItem.getMenu())) {
-                        basketManager.getOrderingItemArrayList().remove(basketItem1);
-                    }
-                }
-                basketManager.getOrderingItemArrayList().add(basketItem);
+            BasketDialog basketDialog = new BasketDialog(MovieOrderingActivity.this, uosPartnerId, basketManager, tvMovieOrderingCompanyName.getText().toString());
+            basketDialog.setOnDismissListener(dialogInterface -> {
                 updatePriceInfo();
+            });
+            basketDialog.show();
+        });
+
+        /* 결제 버튼이 눌렸을 경우 */
+        llMovieOrderingPay.setOnClickListener(view -> {
+            Intent intent = new Intent(MovieOrderingActivity.this, PayActivity.class);
+            intent.putExtra("uosPartnerId", uosPartnerId);
+            intent.putExtra("basketManager", basketManager);
+            intent.putExtra("companyName", tvMovieOrderingCompanyName.getText().toString());
+            startActivity(intent);
+        });
+
+        /* 목록에 있는 영화를 선택했을 경우 */
+        movieOrderingAdapter.setOnItemClickListener((view, position) -> new SelectSeatDialog(MovieOrderingActivity.this, movieOrderingAdapter.getItem(position), (updatedMovieItem, basketItem) -> {
+            movieOrderingAdapter.setItem(position, updatedMovieItem.clone());
+            basketItem.setMovieItem(movieOrderingAdapter.getItem(position));
+
+            for (BasketItem basketItem1 : basketManager.getOrderingItemArrayList()) {
+                if (basketItem1.getMenu().equals(basketItem.getMenu())) {
+                    basketManager.getOrderingItemArrayList().remove(basketItem1);
+                }
             }
+            basketManager.getOrderingItemArrayList().add(basketItem);
+            updatePriceInfo();
         }).show());
     }
 
+    /**
+     * 상품 선택 또는 선택해제 시 선택 상품 정보(가격 및 개수)를 갱신합니다.
+     */
     private void updatePriceInfo() {
         ValueAnimator va = ValueAnimator.ofInt(Integer.valueOf(tvMovieOrderingTotalPrice.getText().toString().replace(",", "")), basketManager.getOrderPrice());
         va.setDuration(1000);
@@ -267,11 +264,17 @@ public class MovieOrderingActivity extends UosActivity {
         tvMovieOrderingProductCount.setText(String.valueOf(basketManager.getOrderCount()));
 
         if (basketManager.getOrderCount() == 0) {
+
+            /* 장바구니에 담긴 상품이 없을 경우 */
+
             llMovieOrderingSelected.setEnabled(false);
             llMovieOrderingSelected.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.gray));
             llMovieOrderingPay.setEnabled(false);
             llMovieOrderingPay.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.gray));
         } else {
+
+            /* 장바구니에 담긴 상품이 있을 경우 */
+
             llMovieOrderingSelected.setEnabled(true);
             llMovieOrderingSelected.setBackgroundColor(getResources().getColor(com.uos.uos_mobile.R.color.color_primary));
             llMovieOrderingPay.setEnabled(true);
